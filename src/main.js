90e4a41e9344693cceacc2bb2e5fde6e6a07acc5
hyden243 <554067218@qq.com>
2022-12-08 22:33:37 +0800

    3-项目基本优化

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // 项目入口文件
 const Koa = require('koa')
+
+const {APP_PORT} = require('./config/config.default')
 const app = new Koa()
 
 // app.use使用中间件  ctx 上下文在上下文点body中插入文字
 app.use((ctx,next)=>{
-    ctx.body = 'hello api'
+    ctx.body = 'hello world!!!'
 })
 
 // 监听4000端口
-app.listen(4000,()=>{
-    console.log('serve is running on http://localhost:4000');
+app.listen(APP_PORT,()=>{
+    console.log(`serve is running on http://localhost:${APP_PORT}`);
 })
