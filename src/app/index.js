432bac3e526da385553c3dbf6a9f179bb28209aa
hyden243 <554067218@qq.com>
2022-12-14 15:27:29 +0800

    06-拆分service

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,12 @@
 // 项目入口文件
 const Koa = require('koa')
+const {koaBody} = require('koa-body')
+
 
 const userRouter = require('../router/user.route')
 
 const app = new Koa()
+app.use(koaBody());
 
 // app.use使用中间件 接收的必须是一个函数 所以要调用routes()函数
 // 注册路由
